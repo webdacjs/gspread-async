910996468fa0e781963302ea4c33188bc476d420
webdacjs <me@jco.im>
2019-07-30 22:15:02 +0200

    Adding settings check for each one of the functions.

--- index.js
+++ index.js
@@ -1,49 +1,82 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
-const {promisify} = require('util')
+const { promisify } = require('util')
+const fs = require('fs')
+const fsAccessAsync = promisify(fs.access)
 const ch = require('console-hue')
 const getSpreadObj = docid => new GoogleSpreadsheet(docid)
 const sheets = {}
 
+const getsCredsPath = filepath => `${process.cwd()}/${filepath}`
+
+async function fileExists (filepath) {
+  try {
+    await fsAccessAsync(filepath, fs.F_OK)
+    return
+  } catch (err) {
+    return err.code
+  }
+}
+
+async function checkSettings (params = {}) {
+  if (!params.credspath || !params.docid && !params.sheetid) {
+    ch.error('Missing spreadsheet parameters check your settings object')
+    return
+  }
+  const credsfileMissing = await fileExists(getsCredsPath(params.credspath))
+  if (credsfileMissing) {
+    ch.error('Missing credentials file.')
+    return
+  }
+  return true
+}
+
 async function authorize (docid, credspath) {
-    const currentPath = process.cwd()
-    const doc = getSpreadObj(docid)
-    const docUseServiceAsync = promisify(doc.useServiceAccountAuth)
-    const docInfoAsync = promisify(doc.getInfo)
-    const creds = require(`${currentPath}/${credspath}`)
-    await docUseServiceAsync(creds)
-    const info = await docInfoAsync()
-    return info.worksheets
+  const doc = getSpreadObj(docid)
+  const docUseServiceAsync = promisify(doc.useServiceAccountAuth)
+  const docInfoAsync = promisify(doc.getInfo)
+  const creds = require(getsCredsPath(credspath))
+  await docUseServiceAsync(creds)
+  const info = await docInfoAsync()
+  return info.worksheets
 }
 
 async function getSheet (params) {
-    if (!sheets[params.sheetid]) {
-        sheets[params.sheetid] = await authorize(params.docid, params.credspath)
-        ch.success('Spreadsheet initialized')
-    }
-    return sheets[params.sheetid].filter(x => x.title === params.sheetid)[0]
+  const check = await checkSettings(params)
+  if (!check) return
+  if (!sheets[params.sheetid]) {
+    sheets[params.sheetid] = await authorize(params.docid, params.credspath)
+    ch.success('Spreadsheet initialized')
+  }
+  return sheets[params.sheetid].filter(x => x.title === params.sheetid)[0]
 }
 
 async function getCells (params) {
-    const options = params.options || {}
-    const sh = await getSheet(params)
-    const getCellsAsync = promisify(sh.getCells)
-    const cells = await getCellsAsync(options)
-    return cells
+  const check = await checkSettings(params)
+  if (!check) return
+  const options = params.options || {}
+  const sh = await getSheet(params)
+  const getCellsAsync = promisify(sh.getCells)
+  const cells = await getCellsAsync(options)
+  return cells
 }
 
 async function getRows (params) {
-    const options = params.options || {}
-    const sh = await getSheet(params)
-    const getRowsAsync = promisify(sh.getRows)
-    const rows = await getRowsAsync(options)
-    return rows
+  const check = await checkSettings(params)
+  if (!check) return
+  const options = params.options || {}
+  const sh = await getSheet(params)
+  const getRowsAsync = promisify(sh.getRows)
+  const rows = await getRowsAsync(options)
+  return rows
 }
 
 async function addRow (params, row) {
-    const sh = await getSheet(params)
-    const addRowAsync = promisify(sh.addRow)
-    const results = await addRowAsync(row)
-    return results
+  const check = await checkSettings(params)
+  if (!check) return
+  const sh = await getSheet(params)
+  const addRowAsync = promisify(sh.addRow)
+  const results = await addRowAsync(row)
+  return results
 }
 
 module.exports = {
