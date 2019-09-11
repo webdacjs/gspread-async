e109c65299cd042f626d6782ef551a9b2e604341
webdacjs <me@jco.im>
2019-09-11 22:59:05 +0200

    Moving getSheet to separate module.

---
+++ getsheet.js
@@ -0,0 +1,30 @@
+const GoogleSpreadsheet = require('google-spreadsheet')
+const { promisify } = require('util')
+const ch = require('console-hue')
+const checkSettings = require('./checksettings')
+const getSpreadObj = docid => new GoogleSpreadsheet(docid)
+const sheets = {}
+
+const getsCredsPath = filepath => `${process.cwd()}/${filepath}`
+
+async function authorize (docid, credspath) {
+  const doc = getSpreadObj(docid)
+  const docUseServiceAsync = promisify(doc.useServiceAccountAuth)
+  const docInfoAsync = promisify(doc.getInfo)
+  const creds = require(getsCredsPath(credspath))
+  await docUseServiceAsync(creds)
+  const info = await docInfoAsync()
+  return info.worksheets
+}
+
+async function getSheet (params) {
+  const check = await checkSettings(params)
+  if (!check) return
+  if (!sheets[params.sheetid]) {
+    sheets[params.sheetid] = await authorize(params.docid, params.credspath)
+    ch.success('Spreadsheet initialized')
+  }
+  return sheets[params.sheetid].filter(x => x.title === params.sheetid)[0]
+}
+
+module.exports = getSheet
