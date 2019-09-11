37eb5f3c0a22605e55f2d9f8eac649e8b3fba00f
webdacjs <me@jco.im>
2019-09-12 00:42:53 +0200

    Removing redundant check.

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const prom = require('./promisifyWrapper')
-const checkSettings = require('./checksettings')
 const getSheet = require('./getsheet')
 
 async function getCells (params) {
-  const check = await checkSettings(params)
-  if (!check) return
-  const options = params.options || {}
   const sh = await getSheet(params)
+  if (!sh) return
+  const options = params.options || {}
   const getCellsAsync = prom(sh.getCells)
   const cells = await getCellsAsync(options)
   return cells
 }
 
 async function getRows (params) {
-  const check = await checkSettings(params)
-  if (!check) return
-  const options = params.options || {}
   const sh = await getSheet(params)
+  if (!sh) return
+  const options = params.options || {}
   const getRowsAsync = prom(sh.getRows)
   const rows = await getRowsAsync(options)
-
   return rows
 }
 
 async function addRow (params, row) {
-  const check = await checkSettings(params)
-  if (!check) return
   const sh = await getSheet(params)
+  if (!sh) return
   const addRowAsync = prom(sh.addRow)
   const results = await addRowAsync(row)
   return results
