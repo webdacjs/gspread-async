1284632699bd99f38c69dc53dd61cbc782e71bcd
webdacjs <me@jco.im>
2019-09-11 22:58:49 +0200

    Moving check settings to separate module.

---
+++ checksettings.js
@@ -0,0 +1,28 @@
+const { promisify } = require('util')
+const fs = require('fs')
+const fsAccessAsync = promisify(fs.access)
+const ch = require('console-hue')
+
+const getsCredsPath = filepath => `${process.cwd()}/${filepath}`
+
+async function fileExists (filepath) {
+  try {
+    await fsAccessAsync(filepath, fs.F_OK)
+    return
+  } catch (err) {
+    return err.code
+  }
+}
+
+module.exports = async function (params = {}) {
+  if (!params.credspath || !params.docid && !params.sheetid) {
+    ch.error('Missing spreadsheet parameters check your settings object')
+    return
+  }
+  const credsfileMissing = await fileExists(getsCredsPath(params.credspath))
+  if (credsfileMissing) {
+    ch.error('Missing credentials file.')
+    return
+  }
+  return true
+}
